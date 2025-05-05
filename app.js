7950865cb65e5abd0e708823c8b57b3ea86a4b5f
Ricardo Hernández <insp3ctionsarms@gmail.com>
2025-05-04 19:48:48 -0600

    Interactividad del frontend

---
+++ app.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require("path");
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Middleware para servir archivos estáticos (CSS, JS, imágenes, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta raíz de prueba
+app.get('/', (req, res) => {
+  res.send('¡Bienvenido al backend!');
+});
+
+// Rutas de usuarios
+/*
+const rutaUsuarios = require('./routes/usuarios');
+app.use('/usuarios', rutaUsuarios);
+
+ */
+
+// Rutas de publicaciones
+/*
+const rutaPublicaciones = require('./routes/publicaciones');
+app.use('/publicaciones', rutaPublicaciones);
+
+ */
+
+// Rutas para servir archivos HTML
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "app/views/login.html"));
+});
+
+app.get('/feed', (req, res) => {
+  res.sendFile(path.join(__dirname, 'app/views/feed.html'));
+});
+
+app.get('/post', (req, res) => {
+  res.sendFile(path.join(__dirname, 'app/views/create_post.html'));
+});
+
+app.get('/detail', (req, res) => {
+  res.sendFile(path.join(__dirname, 'app/views/post_detail.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'app/views/register.html'));
+});
+
+app.get('/perfil', (req, res) => {
+  res.sendFile(path.join(__dirname, 'app/views/profile.html'));
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+
+  console.log(`Servidor escuchando en http://localhost:${PORT}/feed`);
+  console.log(`Servidor escuchando en http://localhost:${PORT}/login`);
+});
---
+++ public/utils.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// Redirección a la página de login
+function goLogin() {
+    window.location.href = "/login";
+}
+
+// Redirección a la página de feed (foro)
+function goFeed() {
+    window.location.href = "/feed";
+}
+
+// Redirección a la página de crear publicación
+function goPost() {
+    window.location.href = "/post";
+}
+
+// Redirección a la página de detalle de publicación
+function goDetail() {
+    window.location.href = "/detail";
+}
+
+// Redirección a la página de registro
+function goRegister() {
+    window.location.href = "/register";
+}
+
+function goPerfil() {
+    window.location.href = "/perfil";
+}
+
