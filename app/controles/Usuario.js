0446af4fd0605e0464043ebcfbe3b4752c7c99a0
Diego <146885057+Necross12@users.noreply.github.com>
2025-05-10 21:04:47 -0600

    Hacer rol enumeracion
    Por el momento no se usa rol pero asi es mas facil de utilizar

--- app/controles/Usuario.js
+++ app/controles/Usuario.js
@@ -1,11 +1,29 @@
 const mongoose = require('mongoose');
 
 const usuarioSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  apellido: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  rol: { type: String, default: 'usuario' }
+  nombre: { 
+    type: String, 
+    required: true 
+  },
+  apellido: { 
+    type: String, 
+    required: true 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  rol: { 
+    type: String, 
+    enum: ['usuario', 'admin'],
+    default: 'usuario' 
+  }
 });
 
 module.exports = mongoose.model('Usuario', usuarioSchema);
+
