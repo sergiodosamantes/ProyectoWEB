126324a789d99bcaf291f07cd52c1d0a0bbd9f8e
sergiodosamantes <sergioidp@hotmail.com>
2025-05-13 09:06:46 -0600

    Cambios para conexion

--- database.js
+++ database.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-  dbName: 'Cluster0',
-});
+      dbName: 'Cluster0' 
+    });
     console.log('Conectado a MongoDB');
   } catch (error) {
     console.error('Error al conectar a MongoDB', error);
